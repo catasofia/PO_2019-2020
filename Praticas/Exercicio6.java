db3b070de78d10ac9ca3fda4625ca4a1231bdc2f
nelsontr <trindade.nelson99@gmail.com>
2019-11-10 18:03:51 +0000

    Organizando Pastas

---
+++ Praticas/Exercicio6.java
@@ -0,0 +1,10 @@
+import java.util
+import java.util
+import java.util
+import java.util
+
+public class Empresa{
+    public void adicionarF(Funcionario funcionario){
+        Iterator<Funcionario> iter = _funcionarios.iterator(); 
+    }
+}
--- Projeto/m19/core/LibraryManager.java
+++ Projeto/m19/core/LibraryManager.java
@@ -1,77 +1,58 @@
-package m19.core;
+public class LibraryManager{
+  private int _nUtentes;
+  private int _nObras;
+  private Utente[] _utentes;
+  private Tempo _tempo;
+  private Requisicoes[] _requisicoes;
 
-import java.io.IOException;
-import java.io.FileNotFoundException;
-
-import m19.core.exception.MissingFileAssociationException;
-import m19.core.exception.BadEntrySpecificationException;
-import m19.core.exception.ImportFileException;
-
-// FIXME import other system types
-// FIXME import other project (core) types
-
-/**
- * The façade class.
- */
-public class LibraryManager {
-
-  private Library _library;  // FIXME initialize this attribute
-
-  // FIXME define other attributes
-
-  // FIXME define contructor(s)
-  
-  // FIXME define methods
-
-  /**
-   * Serialize the persistent state of this application.
-   * 
-   * @throws MissingFileAssociationException if the name of the file to store the persistent
-   *         state has not been set yet.
-   * @throws IOException if some error happen during the serialization of the persistent state
-
-   */
-  public void save() throws MissingFileAssociationException, IOException {
-    // FIXME implement method
+  public LibraryManager(){
+    _nUtentes = 0;
+    _nObras = 0;
+    _tempo = new Tempo();
+    _utentes = new Utente[10];
+    _requisicoes = new Requisicoes[10];
   }
 
-  /**
-   * Serialize the persistent state of this application into the specified file.
-   * 
-   * @param filename the name of the target file
-   *
-   * @throws MissingFileAssociationException if the name of the file to store the persistent
-   *         is not a valid one.
-   * @throws IOException if some error happen during the serialization of the persistent state
-   */
-  public void saveAs(String filename) throws MissingFileAssociationException, IOException {
-    // FIXME implement method
+  public int mostrarData(){
+    return _tempo.obterDia();
   }
 
-  /**
-   * Recover the previously serialized persitent state of this application.
-   * 
-   * @param filename the name of the file containing the perssitente state to recover
-   *
-   * @throws IOException if there is a reading error while processing the file
-   * @throws FileNotFoundException if the file does not exist
-   * @throws ClassNotFoundException 
-   */
-  public void load(String filename) throws FileNotFoundException, IOException, ClassNotFoundException {
-    // FIXME implement method
+  public void avançarData(int nvData){
+    _tempo.alteraDia(nvData);
   }
 
-  /**
-   * Set the state of this application from a textual representation stored into a file.
-   * 
-   * @param datafile the filename of the file with the textual represntation of the state of this application.
-   * @throws ImportFileException if it happens some error during the parsing of the textual representation.
-   */
-  public void importFile(String datafile) throws ImportFileException {
-    try {
-      _library.importFile(datafile);
-    } catch (IOException | BadEntrySpecificationException e) {
-      throw new ImportFileException(e);
-    }
+  public void registarUtente(String nome, String email){
+    _utentes[++_nUtentes] = new Utente(_nUtentes,nome,email);
+  }
+
+  public void mostrarUtente(int iDUtente){
+    _utentes[iDUtente].mostrarUtente();
+  }
+
+  public void mostrarUtentes(){
+    for (int i = 0;i <= _nUtentes; i++)
+      _utentes[i].mostrarUtente();
+  }
+
+  public void mostrarNotificacoes(int iDUtente){
+    _utentes[iDUtente].mostrarNotificacao(0);
+  }
+
+  public void pagarMulta(int iDUtente){}
+
+
+
+
+  public static void main(String argv[]){
+    LibraryManager Library = new LibraryManager();
+
+    Library.registarUtente("Nelson", "trindade.nelson99@gmail.com");
+    //Library._utentes[1]._notificacoes[0] = new Notificacao("ok","User Criado");
+    Library.mostrarUtente(1);
+    Library.mostrarNotificacoes(1);
+    Library.avançarData(2);
+    System.out.println(Library.mostrarData());
+
+    System.out.println(Library._utentes[1].obterPontuacao().obterSituacao());
   }
 }
